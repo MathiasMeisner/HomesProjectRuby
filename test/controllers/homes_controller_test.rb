f43e8639735b6acadab23fbf3c52d9803bb7b0ad
Mathias <mathias.meisner@gmail.com>
2024-04-04 10:37:40 +0200

    Expanded homes_controller_test.rb to include testing of filtering homes.

--- test/controllers/homes_controller_test.rb
+++ test/controllers/homes_controller_test.rb
@@ -6,7 +6,23 @@ class HomesControllerTest < ActionDispatch::IntegrationTest
         get '/api/homes'
         assert_response :success
         assert_not_empty response.body
-        assert_instance_of Array, JSON.parse(response.body)
     end
     
+    test "should return list of filtered homes - price" do
+        get '/api/homes/filter_homes?min_price=3000000&max_price=&min_sqm=&max_sqm=&min_constructionyear=&max_constructionyear='
+        assert_response :success
+        assert_not_empty response.body
+    end
+
+    test "should return list of filtered homes - sqm" do
+        get '/api/homes/filter_homes?min_price=&max_price=&min_sqm=150&max_sqm=&min_constructionyear=&max_constructionyear='
+        assert_response :success
+        assert_not_empty response.body
+    end
+
+    test "should return list of filtered homes - construction year" do
+        get '/api/homes/filter_homes?min_price=&max_price=&min_sqm=&max_sqm=&min_constructionyear=2000&max_constructionyear='
+        assert_response :success
+        assert_not_empty response.body
+    end
 end
